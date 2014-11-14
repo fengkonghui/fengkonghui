bf551ab8328579b12af620a4f24ed25240969b30
shaoyuan <zhangshaoyuan.ceo@gmail.com>
2014-11-14 11:29:40 +0800

    差缺补漏

---
+++ Application/Portal/Controller/ServiceController.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 服务机构
+ */
+namespace Portal\Controller;
+
+use Common\Controller\HomeBaseController;
+
+class ServiceController extends HomeBaseController {
+	public function index() {
+		$this->display();
+	}
+}
